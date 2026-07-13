a50485ed92d39a9d1f71465c5b0ed49b52df9c81
Ashish Vaghela <ashish.vaghela@nelkinda.com>
2026-07-13 13:12:47 +0530

    chore: bump version to 0.2.0

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         applicationId = "com.mindguard"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     if (hasKeystore) {
